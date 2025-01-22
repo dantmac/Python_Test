273d9c1ff61697c828da5e08f3a90cf4ced2619e
Dan Shein <deshein@alfabank.ru>
2025-01-22 10:52:20 +0300

    Implemented func for 'Task #7'

--- Test_1/test_1.py
+++ Test_1/test_1.py
@@ -1,1 +1,17 @@
-# Test
+# Задание 7
+
+def sum_of_factorials(n):
+    if n <= 0:
+        return "Ошибка: вводимое число НЕ должно быть меньше 1!"
+    
+    factorial = 1
+    total_sum = 0
+    
+    for i in range(1, n + 1):
+        factorial *= i
+        total_sum += factorial
+    return total_sum
+
+# Пример использования
+number_n = int(input("Введите натуральное число: "))
+print("Сумма факториалов:", sum_of_factorials(number_n))
